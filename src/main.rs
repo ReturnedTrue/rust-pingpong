cfa75d2e5f2315a3b2d927bd82c5bc900f9003da
ReturnedTrue <developerbilly20@gmail.com>
2020-09-16 21:07:01 +0100

    Draw background meshes and move to Point2 structs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-#[ignore(unused_parens)]
+#![allow(unused_parens)]
+#![allow(unused_variables)]
+#![allow(non_snake_case)]
+#![allow(dead_code)]
 
 extern crate ggez;
 extern crate rand;
 
-use ggez::{graphics, event, conf, Context, ContextBuilder, GameResult};
+use ggez::{graphics, event, conf, nalgebra::Point2, Context, ContextBuilder, GameResult};
 use rand::Rng;
 
 struct Object {
-    position: [f32; 2],
-    velocity: [f32; 2]     
+    position: Point2<f32>,
+    velocity: Point2<f32>
 }
 
 struct Player {
@@ -17,60 +20,75 @@ struct Player {
 }
 
 struct Game {
-    context: &mut Context,
+    window_size: Point2<f32>,
     ball: Object,
     p1: Player,
     p2: Player
 }
 
 impl Game {
-    pub fn new(context: &mut Context) -> Game {
+    pub fn new(context: &Context) -> Game {
+        let (x, y) = graphics::size(context);
+
         return Game {
-            context: context,
+            window_size: Point2::new(x, y),
+
             ball: Object {
-                position: [0.0, 0.0],
-                velocity: [0.0, 0.0]
+                position: Point2::new(0.0, 0.0),
+                velocity: Point2::new(0.0, 0.0)
             },
             
             p1: Player {
                 score: 0,
                 paddle: Object {
-                    position: [0.0, 0.0],
-                    velocity: [0.0, 0.0]   
+                    position: Point2::new(0.0, 0.0),
+                    velocity: Point2::new(0.0, 0.0)  
                 }
             },
             
             p2: Player {
                 score: 0,
                 paddle: Object {
-                    position: [0.0, 0.0],
-                    velocity: [0.0, 0.0]   
+                    position: Point2::new(0.0, 0.0),
+                    velocity: Point2::new(0.0, 0.0)   
                 }
             }
         };
     }
 
-    pub fn random(min: i32, max: i32) -> i32 {
-        return rand::thread_rng().gen_range(min, max);
-    }
-
-    pub fn start(&mut self) {
+    pub fn start(&mut self, context: &mut Context, events_loop: &mut event::EventsLoop) {
+        match event::run(context, events_loop, self) {
+            Ok(_) => println!("Success!"),
+            Err(error) => println!("An error occurred: {}", error)
+        }
         
+        if (self.random(1, 2) == 1) {
+            self.create_ball(false, context);
+        } else {
+            self.create_ball(true, context);
+        }
+
+
     }
 
-    fn create_ball(&mut self, right_side: bool) {
-        let (x, y) = graphics::size(&self.context);
-        let (mut horizontal, vertical) = (self::random(2, 4), self::random(1, 3));
-
-        self.ball.position = [x / 2.0, y / 2.0];
+    fn create_ball(&mut self, right_side: bool, context: &mut Context) {
+        let (mut horizontal, vertical) = (self.random(2.0, 4.0), self.random(1.0, 3.0));
+        
+        self.ball.position = Point2::new(self.window_size.x / 2.0, self.window_size.y / 2.0);
         
         if (right_side) {
             horizontal = -horizontal;
         }
 
-        self.ball.velocity = [horizontal, vertical];
+        self.ball.velocity = Point2::new(horizontal, vertical);
+    }
+
+    
+    fn random<T: rand::distributions::uniform::SampleUniform>(&mut self, min: T, max: T) -> T {
+        return rand::thread_rng().gen_range(min, max);
     }
 }
+
 
 impl event::EventHandler for Game {
     fn update(&mut self, context: &mut Context) -> GameResult<()> {
@@ -80,6 +98,25 @@ impl event::EventHandler for Game {
 
     fn draw(&mut self, context: &mut Context) -> GameResult<()> {
         graphics::clear(context, graphics::BLACK);
+
+        let LINE_WIDTH = self.window_size.x / 100.0; // 1% of screen
+        let LINE_COLOR = graphics::WHITE;
+
+        let MIDDLE_X = (self.window_size.x / 2.0) - LINE_WIDTH;
+        let END_X = self.window_size.x - (LINE_WIDTH * 2.0);
+
+        let backgroundMesh = graphics::MeshBuilder::new()
+            .line(&[ Point2::new(MIDDLE_X, 0.0), Point2::new(MIDDLE_X, self.window_size.y) ], LINE_WIDTH, LINE_COLOR)? // Middle
+            .line(&[ Point2::new(0.0, 0.0), Point2::new(0.0, self.window_size.y) ], LINE_WIDTH, LINE_COLOR)? // Left
+            .line(&[ Point2::new(END_X, 0.0), Point2::new(END_X, self.window_size.y) ], LINE_WIDTH, LINE_COLOR)? // Right
+            .circle(graphics::DrawMode::fill(), Point2::new(self.window_size.x / 2.0 - LINE_WIDTH, self.window_size.y / 2.0), self.window_size.x * 0.05, 0.05, LINE_COLOR)
+            .build(context)?;
+
+        graphics::draw(
+            context,
+            &backgroundMesh,
+            graphics::DrawParam::default()
+        )?;
 
 
         return graphics::present(context);
@@ -91,7 +128,7 @@ fn main() {
         width: 800.0,
         height: 600.0,
         maximized: true,
-        fullscreen_type: conf::FullscreenType::Desktop,
+        fullscreen_type: conf::FullscreenType::Windowed,
         borderless: false,
         min_width: 0.0,
         max_width: 0.0,
@@ -108,7 +145,7 @@ fn main() {
         srgb: true
     };
 
-    let (mut context, mut event_loop) = ContextBuilder::new("Ping Pong", "ReturnedTrue")
+    let (mut context, mut events_loop) = ContextBuilder::new("Ping Pong", "ReturnedTrue")
         .window_mode(mode)
         .window_setup(setup)
         .build()
@@ -116,10 +153,7 @@ fn main() {
 
     let mut my_game = Game::new(&context);
 
-    match event::run(&mut context, &mut event_loop, &mut my_game) {
-        Ok(_) => println!("Success!"),
-        Err(error) => println!("An error occurred: {}", error)
-    }
+    my_game.start(&mut context, &mut events_loop);
 }
 
-// TODO: Fix finding random in self and add lifetime specifier
+// TODO: Move onto paddle / text drawing and key handling
